bcb19ebf97f5665d59bdc5f94d32a263aad8dcfd
rohanjadhav2 <93446293+rohanjadhav2@users.noreply.github.com>
2021-10-30 23:39:26 +0530

    Add files via upload

---
+++ difference.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,diff;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    printf("enter the value of b");
+    scanf("%d",&b);
+    diff=a-b;
+    printf("%d",diff);
+    return 0;
+}
